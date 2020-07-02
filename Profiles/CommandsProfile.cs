6d348c8dc23313f307a44953b48f4f971ef0869f
cassionunes@cofcointernational.com <cassionunes@cofcointernational.com>
2020-07-02 16:47:46 +0100

    Added comments as example

--- Profiles/CommandsProfile.cs
+++ Profiles/CommandsProfile.cs
@@ -14,6 +14,19 @@ namespace Commander.Profiles
             CreateMap<CommandUpdateDto, Command>();
             CreateMap<Command, CommandUpdateDto>();
         }
+        
+    //  Mapper.CreateMap<StockCheckViewModel, StockCheck>()
+    // .ForMember(m => m.Id, opt => opt.Ignore())
+    // .ForMember(vm => vm.StockCheckItems, opt => opt.Ignore())
+    // .ForMember(vm => vm.Remarks, opt => opt.MapFrom(m => string.IsNullOrEmpty(m.Remarks) ? "" : m.Remarks))
+    // .ForMember(vm => vm.RecordStatus, opt => opt.UseValue(ModelConstant.RecordStatusActive))
+    // .AfterMap((model, entity) => UpdateStockCheckItemEntity(entity.StockCheckItems, model.StockCheckItems));
+
+
+//TODO 
+// - improve the profile config cases
+// - add unit test using IMapper
+
 
     }
     
